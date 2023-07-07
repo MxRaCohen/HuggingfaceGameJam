ae0361a844dfc81cc803bac8f57d6c35a2bef2b3
pkamali30 <133684125+pkamali30@users.noreply.github.com>
2023-07-07 15:07:03 -0400

    Action points implemented

--- run.py
+++ run.py
@@ -24,11 +24,14 @@ circle_destinations = list(circle_positions)  # Start destinations as the initia
 circle_colors = [random.choice(['red', 'blue', 'green']) for _ in range(num_circles)]  # Assign random colors
 
 # Speed of the circles
-speed = 300
+speed = 2000
 
 # Variable to store which circle is currently being controlled
 current_circle = None
 dragging = False
+
+# Action point variable
+action_points = 5  # Set the desired number of action points
 
 def is_point_in_circle(point, circle_center, circle_radius):
     return (point - circle_center).length() <= circle_radius
@@ -54,6 +57,8 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONUP:
             dragging = False
+            if current_circle is not None and action_points > 0:
+                action_points -= 1
 
         elif event.type == pygame.MOUSEMOTION:
             if dragging and current_circle is not None:
@@ -86,6 +91,16 @@ while running:
     # Draw all circles with their assigned colors
     for i in range(num_circles):
         pygame.draw.circle(screen, circle_colors[i], circle_positions[i], circle_radius)
+        
+    if action_points == 0:
+        message = font.render("You are out of moves!", True, pygame.Color("white"))
+        message_rect = message.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+        screen.blit(message, message_rect)
+
+    # Display action points
+    font = pygame.font.SysFont(None, 36)
+    text = font.render(f"Actions: {action_points}", True, pygame.Color("white"))
+    screen.blit(text, (10, 10))
 
     pygame.display.flip()
 
